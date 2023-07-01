f322bbf26c25cc176e01815db26c259c8ce0e6c8
AndreyPlatonov <one.and.pl@gmail.com>
2023-07-01 19:54:55 +0300

    fix dz task 1

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -256,5 +256,4 @@ public class CallbackTest {
 
     }
 
-
 }
--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -4,10 +4,20 @@ import com.github.javafaker.Faker;
 
 import java.util.Locale;
 
+import static com.google.common.base.Ascii.toLowerCase;
+
+
 public class DataGenerator {
 
     public static RegistrationByClientInfo generatedByInfo() {
         Faker faker = new Faker(new Locale(("ru")));
+
+        while (toLowerCase(faker.name().lastName()+faker.name().firstName()).contains("ё") ){
+
+            Faker fakerTmp = new Faker(new Locale(("ru")));
+
+            faker=fakerTmp;
+        }
         return new RegistrationByClientInfo(
                 faker.address().city(),
                 faker.name().lastName() + " " + faker.name().firstName(),
